65b31d48c0edefb0f037b3cc7ed48bf8cb75a437
sabarishraj <35728456+sabarishraj@users.noreply.github.com>
2018-02-06 16:36:40 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int c=10;
+int d=20;
+printf("enter the value a");
+scanf("%d",&a);
+printf("enter the value b");
+scanf("%d",&b);
+printf("swapping the values");
+a=a+b;
+b=a-b;
+a=a-b;
+printf("\n after swap s=%d b=%d (a,b));
+}
