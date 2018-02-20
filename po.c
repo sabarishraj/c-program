2fb1b0cd4ed104e00e155300fb5505fa957aff8b
sabarishraj <35728456+sabarishraj@users.noreply.github.com>
2018-02-20 16:19:51 +0530

    Create po.c

---
+++ po.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n>0)
+{
+printf("The no is positive");
+}
+else if(n<0)
+{
+printf("The no is negative");
+}
+else
+{
+printf("The no is equal to 0");
+}
