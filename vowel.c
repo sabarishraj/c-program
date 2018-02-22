6808e54dc5ecad5957526fa2f641a2ae2d4e392f
sabarishraj <35728456+sabarishraj@users.noreply.github.com>
2018-02-22 16:19:05 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -2,11 +2,9 @@
 void main()
 {
   char k;
-  
   printf("Enter a character to know whether it is vowel or not\n");
   
   scanf("%c",&k);
-  
   if(k=='A'||c=='a'||k=='e'||k=='E'||k=='I'||k=='i'||k=='O'||k=='o'||k=='U'||k=='u')
   {
     printf("The character %c is vowel\n",k);
