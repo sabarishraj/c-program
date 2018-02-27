a46414a7f25251f71068ed6525ee2a288798ddd9
sabarishraj <35728456+sabarishraj@users.noreply.github.com>
2018-02-27 16:29:06 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+char s[10];
+int n;
+int i;
+scanf("%s",&s);
+printf("number of times");
+scanf("%d",&n);
+
+for(i=1;i<n;i++)
+{
+printf("%s",s);
+printf("\n");
+}
+}
